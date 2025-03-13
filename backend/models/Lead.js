30674b29a975e86d8e0b544cd11377715d00198d
maxkolt <12345kolt@gmail.com>
2025-03-13 23:36:56 +0300

    init

--- backend/models/Lead.js
+++ backend/models/Lead.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const leadSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  phone: { type: String, required: true }
-});
+  phone: { type: String, required: true },
+}, { timestamps: true });
 
 module.exports = mongoose.model('Lead', leadSchema);
--- backend/routes/leadRoutes.js
+++ backend/routes/leadRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Lead = require('../models/leadModel'); // Проверь правильность пути
+const Lead = require('../models/Lead'); // Проверь правильность пути
 
 // ✅ Получение всех лидов
 router.get('/', async (req, res) => {
