0f3c112bf46063350ca980d807bb7bdd4a60ff79
maxkolt <12345kolt@gmail.com>
2025-03-13 23:57:38 +0300

    init

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
 
-// Пример массива категорий (исправлено свойство title вместо name)
-const categories = [
-  { id: 1, title: 'Ремонт и обслуживание cтиральных машин', img: require('../assets/images/images-removebg-preview.png')},
-  { id: 2, title: 'Ремонт и обслуживание посудомоечных машин', img: require('../assets/images/посудомойка-removebg-preview.png')},
-  { id: 3, title: 'Ремонт и обслуживание холодильников', img: require('../assets/images/Без_названия-removebg-preview.png')},
-  { id: 4, title: 'Диагностика поломок любых видов техники', img: require('../assets/images/диагностика-removebg-preview.png')},
-  { id: 5, title: 'Ремонт и обслуживание духовых шкафов', img: require('../assets/images/Без_названия__1_-removebg-preview.png')},
-  { id: 6, title: 'Ремонт и обслуживание кондиционеров', img: require('../assets/images/кондиционер-removebg-preview.png')},
-  { id: 7, title: 'Ремонт и обслуживание вытяжек', img: require('../assets/images/вытяжка-removebg-preview.png') },
-  { id: 8, title: 'Ремонт и обслуживание кофеварок', img: require('../assets/images/кофеварка-removebg-preview.png')},
-  { id: 9, title: 'Ремонт и обслуживание пылесосов', img: require('../assets/images/пылисос-removebg-preview.png')},
-  { id: 10, title: 'Ремонт и обслуживание сушилок', img: require('../assets/images/сушилка-removebg-preview.png')},
-  { id: 11, title: 'Ремонт и обслуживание тостеров', img: require('../assets/images/тостер-removebg-preview.png')},
-  { id: 12, title: 'Установка оборудования', img: require('../assets/images/установка-removebg-preview.png')},
-  { id: 13, title: 'Ремонт и обслуживание электрочайников', img: require('../assets/images/чайник-removebg-preview.png')},
-  { id: 14, title: 'Ремонт и обслуживание электроплит', img: require('../assets/images/электоплита-removebg-preview.png')},
-  { id: 15, title: 'Ремонт и обслуживание телевизоров', img: require('../assets/images/телевизор-removebg-preview.png') },
+// Пример массива категорий (используем статический импорт изображений)
+import image1 from '../assets/images/images-removebg-preview.png';
+import image2 from '../assets/images/removebg-preview.png';
+import image3 from '../assets/images/Без_названия-removebg-preview.png';
+import image4 from '../assets/images/диагностика-removebg-preview.png';
+import image5 from '../assets/images/Без_названия__1_-removebg-preview.png';
+import image6 from '../assets/images/кондиционер-removebg-preview.png';
+import image7 from '../assets/images/вытяжка-removebg-preview.png';
+import image8 from '../assets/images/кофеварка-removebg-preview.png';
+import image9 from '../assets/images/пылисос-removebg-preview.png';
+import image10 from '../assets/images/сушилка-removebg-preview.png';
+import image11 from '../assets/images/тостер-removebg-preview.png';
+import image12 from '../assets/images/установка-removebg-preview.png';
+import image13 from '../assets/images/чайник-removebg-preview.png';
+import image14 from '../assets/images/электоплита-removebg-preview.png';
+import image15 from '../assets/images/телевизор-removebg-preview.png';
 
+const categories = [
+  { id: 1, title: 'Ремонт и обслуживание cтиральных машин', img: image1 },
+  { id: 2, title: 'Ремонт и обслуживание посудомоечных машин', img: image2 },
+  { id: 3, title: 'Ремонт и обслуживание холодильников', img: image3 },
+  { id: 4, title: 'Диагностика поломок любых видов техники', img: image4 },
+  { id: 5, title: 'Ремонт и обслуживание духовых шкафов', img: image5 },
+  { id: 6, title: 'Ремонт и обслуживание кондиционеров', img: image6 },
+  { id: 7, title: 'Ремонт и обслуживание вытяжек', img: image7 },
+  { id: 8, title: 'Ремонт и обслуживание кофеварок', img: image8 },
+  { id: 9, title: 'Ремонт и обслуживание пылесосов', img: image9 },
+  { id: 10, title: 'Ремонт и обслуживание сушилок', img: image10 },
+  { id: 11, title: 'Ремонт и обслуживание тостеров', img: image11 },
+  { id: 12, title: 'Установка оборудования', img: image12 },
+  { id: 13, title: 'Ремонт и обслуживание электрочайников', img: image13 },
+  { id: 14, title: 'Ремонт и обслуживание электроплит', img: image14 },
+  { id: 15, title: 'Ремонт и обслуживание телевизоров', img: image15 },
 ];
 
 function Hero() {
@@ -40,7 +55,7 @@ function Hero() {
                 alt={cat.title}
                 className="w-full h-48 object-contain mb-4"
               />
-              {/* Заголовок и описание */}
+              {/* Заголовок */}
               <h3 className="text-lg">{cat.title}</h3>
             </div>
           ))}
