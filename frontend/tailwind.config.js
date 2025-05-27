74a7d1160742501198f0556ef8b1fc50419157d8
maxkolt <12345kolt@gmail.com>
2025-05-28 03:38:12 +0700

    init

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // <- это нужно для поиска классов Tailwind
+    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  safelist: [
+    "text-center",
+    "text-xl",
+    "font-bold",
+    "flex",
+    "items-center",
+    "justify-between",
+    "bg-red-500",
+    "bg-blue-500",
+    "text-white",
+    "rounded",
+    "p-4"
   ],
   theme: {
     extend: {},
