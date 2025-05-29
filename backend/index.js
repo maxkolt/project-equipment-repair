9ced11425f22cdc7914e48bf73c10411d9d00439
maxkolt <12345kolt@gmail.com>
2025-05-29 20:48:47 +0700

    init

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,6 @@ app.use(express.json());
 
 // MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
 }).then(() => console.log('✅ MongoDB подключена'))
   .catch(err => console.error('❌ Ошибка подключения MongoDB:', err));
 
